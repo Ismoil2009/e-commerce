cf5e4c46703a1483d98085c848bbf1970bb7ed42
Ismoil2009 <osnovavbs092009@gmail.com>
2023-09-01 12:22:06 +0500

    pagination

--- js/products.js
+++ js/products.js
@@ -1,12 +1,14 @@
 const allRow = document.querySelector(".all-row");
 const searchInput = document.querySelector(".search-input");
 const totalProducts = document.querySelector(".allProductsTotal");
+const pagination = document.querySelector(".pagination");
 
 let search = "";
 
+let activePage = 1;
+
 function getAllCard({ id, images, name, description }) {
   let check = cart.find((pr) => pr.id === id);
-
   return `
     <div class="all-card">
       <img
@@ -29,21 +31,52 @@ function getAllProducts() {
       pr.description.toLowerCase().includes(search)
   );
 
+  let pages = Math.ceil(res.length / 10);
+
   totalProducts.textContent = res.length;
+
+  if (pages > 1) {
+    pagination.innerHTML = `
+      <li class="page-item disabled">
+        <button class="page-link">Previous</button>
+      </li>
+    `;
+
+    for (let i = 1; i <= pages; i++) {
+      pagination.innerHTML += `
+        <li class="page-item ${i === activePage ? "active" : ""}">
+          <button onclick="getPage(${i})" class="page-link">${i}</button>
+        </li>
+      `;
+    }
+
+    pagination.innerHTML += `
+      <li class="page-item">
+        <button class="page-link">Next</button>
+      </li>
+    `;
+  } else {
+    pagination.innerHTML = "";
+  }
+
+  let start = (activePage - 1) * 10;
+  let end = activePage * 10;
+
+  let pageResults = res.slice(start, end);
 
   allRow.innerHTML = "";
 
   if (res.length !== 0) {
-    res.map((product) => {
+    pageResults.map((product) => {
       allRow.innerHTML += getAllCard(product);
     });
   } else {
     allRow.innerHTML = `<div style="
       padding: 15px 20px;
       width: 100%;
-      color: yellowgreen;
+      color: black;
       font-size: 16px;
-      border: 2px solid yellowgreen;
+      border: 2px solid black;
       border-radius: 5px";
     >No products !</div>`;
   }
@@ -51,6 +84,7 @@ function getAllProducts() {
 getAllProducts();
 
 searchInput.addEventListener("keyup", function () {
+  activePage = 1;
   search = this.value.trim().toLowerCase();
   getAllProducts();
 });
@@ -72,4 +106,9 @@ function addToCart(id) {
   }
   localStorage.setItem("cart", JSON.stringify(cart));
   getCartTotal();
+}
+
+function getPage(page) {
+  activePage = page;
+  getAllProducts();
 }
